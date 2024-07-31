984f63704dc7907b71509899c2997e477be81f4a
DavideSuzzarellu <davide.suzzarellu@hotmail.com>
2024-07-31 15:47:14 +0200

    fix: SkillsCards and adjust margin

---
+++ src/styles/styles.ts
@@ -0,0 +1,30 @@
+import GithubIcon from "../components/icons/GithubIcon.astro";
+
+export const iconStyles: { [key: string]: string } = {
+    ReactIcon: "bg-[#021F71]  border-[#197c9f] text-[#197c9f] shadow-[#197c9f]",
+    TypescriptIcon: "bg-[#41F5F9]  border-[#1976d2] text-[#1976d2] shadow-[#1976d2]",
+    JavascriptIcon: "bg-[#885206]  border-slate-900 text-[#ffd600] shadow-[#ffd600]",
+    AstroIcon: "bg-white  border-black text-black shadow-[#FF5D01]",
+    BoostrapIcon: "bg-[#E9F2E0]  border-[#6C19FF] text-[#6C19FF] shadow-[#6C19FF]",
+    CSSIcon: "bg-[#C5EFF7]  border-[#2965f1] text-[#2965f1] shadow-[#2965f1]",
+    FlaskIcon: "bg-[#78716c]  border-white text-white shadow-white",
+    HTMLIcon: "bg-[#350C09]  border-[#E65100] text-[#E65100] shadow-[#E65100]",
+    SQLAlchemyIcon: "bg-slate-200  border-[#333] text-[#333] shadow-[#ca2727]",
+    TailwindIcon: "bg-[#254253]  border-[#00ACC1] text-[#00ACC1] shadow-[#00ACC1]",
+    MapsIcon: "bg-[#4B2525]  border-[#fcc60e] shadow-[#48B564] text-[#48B564]",
+    CloudinaryIcon: "bg-[#0C3003]  border-[#DC8327] text-[#0071CE] shadow-[#DC8327]",
+    PythonIcon: "bg-[#244919] border-[#0277BD] shadow-[#FFC107] text-[#FFC107]",
+    ReactRouterIcon: "bg-[#F8BCBC] border-[#D0021B] shadow-[#D0021B] text-[#D0021B]",
+    NodeJSIcon: "bg-[#06b485] border-[#37474F] text-[#37474F] shadow-[#388e3c]",
+    ExpressIcon: "bg-slate-300 border-slate-600 text-black shadow-white",
+    FirebaseIcon: "bg-[#823B07]  border-[#ff8f00] text-[#ff8f00] shadow-[#ff8f00]",
+    ReactBootstrapIcon: "bg-[#2E4A8B] border-[#41E0FF] text-[#41E0FF] shadow-[#41E0FF]",
+    JWTIcon: "bg-[#020D42] border-[#29b6f6] text-[#D500F9] shadow-[#f50057]",
+    StripeIcon: "bg-[#0B1922] border-[#6673FF] text-[#6673FF] shadow-[#6673FF]",
+    PostgresSQLIcon: "bg-[#ACF3F3] border-[#336791] text-[#336791] shadow-[#336791]",
+    GitIcon: "bg-[#0d2b26] border-[#F4511E] text-[#F4511E] shadow-[#F4511E]",
+    GithubIcon: "bg-[#eb9500] border-black text-black shadow-[#eb9500]",
+    VisualStudioIcon: "bg-[#b0dfaf] border-[#561b90] text-[#561b90] shadow-[#b0dfaf]",
+    default: "bg-[#78716c] shadow-white px-3 py-1.5"
+  };
+  
